33576290cd5d2b323ee559d987262f63009bae1f
begouravdev <begouravdev@gmail.com>
2023-10-31 01:14:11 +0530

    showing order items

--- index.js
+++ index.js
@@ -244,6 +244,35 @@ app.get("/generatePdf", async (request, response) => {
         price: 100,
       },
     ];
+
+    let orderInfo = {
+      orderNo: "15484659",
+      invoiceNo: "MH-MU-1077",
+      invoiceDate: "11/05/2021",
+      invoiceTime: "10:57:00 PM",
+      products: [
+        {
+          id: "15785",
+          name: "Case - Bag - White Grocery Bag #6",
+          price: "$30.00",
+          qty: 1,
+        },
+        {
+          id: "15786",
+          name: "Dell Magic Mouse WQ1545",
+          price: "$30.00",
+          qty: 2,
+        },
+      ],
+      totalValue: "$60.00 ",
+    };
+
+    const terms =
+      "By signing this document I/We acknowledge the receipt of invoiced products. I/We agree to pay a finance charge of 1.5%per month on all past due accounts. Umami will charge a $30 processing fee on all returned checks for ACH Customers. A $15 charge will be assessed on order canceled on same day or on second delivery attempt. Claims must be made upon the time of delivery. Please weigh and inspect all items with the driver upon delivery.";
+
+    let fontNormal = "Helvetica";
+    let fontBold = "Helvetica-Bold";
+
     // Create a new PDF document
     const doc = new PDFDocument();
     // Pipe the PDF to the response object
@@ -339,6 +368,46 @@ app.get("/generatePdf", async (request, response) => {
     // });
 
     // Finalize the PDF
+    doc.lineWidth(0.5);
+    doc.strokeColor("#fd8c02");
+    doc.moveTo(50, 300).lineTo(570, 300).stroke();
+
+    doc.rect(50, 310, 520, 24).fill("#ffe8cc").stroke("#fd8c02");
+    doc.fillColor("#fd8c02").font(fontBold).text("QTY", 60, 317, { width: 90 });
+    doc.font(fontBold).text("ITEMS", 110, 317, { width: 300 });
+    doc.font(fontBold).text("RATE", 400, 317, { width: 200 });
+    doc.font(fontBold).text("AMOUNT", 500, 317, { width: 100 });
+
+    let productNo = 1;
+    orderInfo.products.forEach((element) => {
+      let y = 330 + productNo * 20;
+      doc
+        .fillColor("#000")
+        .font(fontNormal)
+        .text(element.qty, 60, y, { width: 90 });
+      doc.font(fontNormal).text(element.name, 110, y, { width: 300 });
+      doc.font(fontNormal).text("", 400, y, { width: 100 });
+      doc.font(fontNormal).text(element.price, 500, y, { width: 100 });
+      productNo++;
+    });
+
+    productNo++;
+
+    doc.dash(5, { space: 2 });
+    doc.lineWidth(0.5);
+    doc.strokeColor("#333333");
+    doc.moveTo(50, 390).lineTo(570, 390).stroke();
+    doc.undash();
+
+    doc.font(fontBold).text("BALANCE DUE", 400, 330 + productNo * 17);
+    doc.font(fontBold).text(orderInfo.totalValue, 500, 330 + productNo * 17);
+
+    doc
+      .fillColor("#000000")
+      .font(fontNormal)
+      .fontSize(11)
+      .text(terms, 60, 500, { continued: true });
+
     doc.end();
 
     // Set response headers for PDF
